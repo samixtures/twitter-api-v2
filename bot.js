a752fb0defc20f9af9cc3012b213142e544cce0b
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-06-19 02:00:45 -0700

    cleaned up bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,4 @@
-console.log("Hello from bot.js!")
 import { TwitterApi } from 'twitter-api-v2';
-console.log("Modules!");
-import config1 from './config1.js';
 
 const client = new TwitterApi('AAAAAAAAAAAAAAAAAAAAADLRdgEAAAAA4%2BY7J%2BNoF9EveIPPzAMnlede%2Fso%3DdnAvCd4lmwCnE3tNjNaAnoRGukCb6Ef5jo6Y8yNMZYh6Uac01i');
 
